347b063b0220603758f83fa786ed6d8f2fc16eb3
Sanchez <sanchezotavio@gmail.com>
2022-10-21 19:44:01 -0300

    feat: configuração prettier e husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,108 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
   ],
-  overrides: [
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: "./tsconfig.json",
   },
   plugins: [
-    'react'
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    'prettier'
   ],
   rules: {
+    camelcase: 'off',
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: [
+          'PascalCase'
+        ],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true
+        }
+      }
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          'module',
+          '/^@shared/',
+          [
+            'parent',
+            'sibling',
+            'index'
+          ]
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true
+        }
+      }
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.spec.js'
+        ]
+      }
+    ],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.jsx',
+          '.tsx'
+        ]
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error'
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
   }
 }
